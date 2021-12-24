05e95eb5f82441f9af902d0c36627f3aa69cacb6
Thanh Hồng <64731516+nongthanhong@users.noreply.github.com>
2021-12-24 11:47:38 +0700

    update_1

---
+++ Unet/main.py
@@ -0,0 +1,7 @@
+from model import *
+from train import *
+from data import *
+
+
+# Free up RAM in case the model definition cells were run multiple times
+keras.backend.clear_session()
---
+++ Unet/train.py
@@ -0,0 +1,14 @@
+from model import *
+
+# Configure the model for training.
+# We use the "sparse" version of categorical_crossentropy
+# because our target data is integers.
+model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("oxford_segmentation.h5", save_best_only=True)
+]
+
+# Train the model, doing validation at the end of each epoch.
+epochs = 15
+model.fit(train_gen, epochs=epochs, validation_data=val_gen, callbacks=callbacks)

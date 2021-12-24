da2104f0a5cea687a869fd03e94feca7296e3b6e
Thanh Hồng <64731516+nongthanhong@users.noreply.github.com>
2021-12-24 12:44:39 +0700

    Update model.py

--- Unet/model.py
+++ Unet/model.py
@@ -49,10 +49,11 @@ def unet(pretrained_weights = None,input_size = (256,256,3), n_classes = 10):
         merge9 = concatenate([conv1,up9], axis = 3)
         conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
         conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-        conv9 = Conv2D(n_classes, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-        conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+        conv9 = Conv2D(n_classes, 3, activation = 'sigmoid', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+        model = Model(input = inputs, output = conv9)
+        #conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
-        model = Model(input = inputs, output = conv10)
+        # model = Model(input = inputs, output = conv10)
         
 
         model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])

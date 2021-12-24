d3b697cf35bc8108bcd49d8131b5c3c5a01864b0
Thanh Hồng <64731516+nongthanhong@users.noreply.github.com>
2021-12-24 15:45:07 +0700

    update 1

--- Unet/main.py
+++ Unet/main.py
@@ -1,7 +1,27 @@
 from model import *
-from train import *
 from data import *
 
 
+
+
+
+
+
+train_path = ""
+test_path = ""
+
+
 # Free up RAM in case the model definition cells were run multiple times
-keras.backend.clear_session()
+tf.keras.backend.clear_session()
+
+img_height = 256
+img_width = 256
+num_channels = 3
+filters = 32
+n_classes = 13
+
+model = unet((img_height, img_width, num_channels), filters=32, n_classes=23)
+model.summary()
+
+tf.keras.utils.plot_model(model, show_shapes=True)
+
